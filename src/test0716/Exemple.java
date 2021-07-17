aa03877f96c1c4c95b0389101d170b7a2422c538
test0716 <test0716@gmail.com>
2021-07-17 15:26:01 +0800

    mimiedit 0717 15:25

--- src/test0716/Exemple.java
+++ src/test0716/Exemple.java
@@ -5,5 +5,6 @@ public class Exemple {
 	public static void main(String[] args) {
 		System.out.println("lulu is cute");
 		System.out.println("mimi");
+		System.out.println("mimiedit 0717 15:25");
 	}
 }
